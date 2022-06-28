0052ba8cbc21cc030ba446fc108070d3e60951e2
Noemi Moctezuma <l.gama000211@gmail.com>
2022-06-28 16:56:08 -0500

    arduino

---
+++ huerto/huerto.ino
@@ -0,0 +1,26 @@
+template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; }
+
+float tempC; 
+int luz;
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  //puerto analogico 9 para sensor de temperatura
+  tempC = analogRead(1); 
+   
+  // Calculamos la temperatura con la fórmula
+  tempC = (5.0 * tempC * 100.0)/1024.0; 
+
+  // puerto A1 para fotoresitencia
+  luz = analogRead(0);
+
+//se imprimen los dos datos
+  Serial << tempC << " " << luz;
+  Serial.println();
+  delay(1000);
+}
