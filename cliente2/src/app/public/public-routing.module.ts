239f0106636a38a5e04656b7c87c046191a11fe7
Noemi Moctezuma <l.gama000211@gmail.com>
2022-08-23 01:05:28 -0500

    html de login, no funciona el formulario

--- cliente2/src/app/public/public-routing.module.ts
+++ cliente2/src/app/public/public-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'prefix',
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./login/login.module').then((m) => m.LoginModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
