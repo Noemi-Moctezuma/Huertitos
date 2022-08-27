1d8739e4a0b779df8d72b9ced878bce6337b58a7
unknown <l.zaol000305@itses.edu.mx>
2022-08-26 22:41:21 -0500

    minimizar el sidenav, eliminados componentes no usados

--- cliente2/src/app/app-routing.module.ts
+++ cliente2/src/app/app-routing.module.ts
@@ -10,6 +10,11 @@ const routes: Routes = [
     path: '',
     loadChildren:() => import('./public/public.module').then((m)=> m.PublicModule),
   }, 
+  // ruta provisional para el dashboard
+  {
+    path: 'dashboard',
+    redirectTo: '/private/dashboard'
+  }
 ];
 
 @NgModule({
--- cliente2/src/app/private/private.component.ts
+++ cliente2/src/app/private/private.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {ChangeDetectorRef, OnDestroy} from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import { ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-private',
   templateUrl: './private.component.html',
-  styleUrls: ['./private.component.scss']
+  styleUrls: ['./private.component.scss'],
 })
-export class PrivateComponent implements OnInit, OnDestroy{
-
+export class PrivateComponent implements OnInit, OnDestroy {
+  isMaximized: boolean = true;
   mobileQuery: MediaQueryList;
   titulo: string = '';
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+  fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
 
   fillerContent = Array.from(
-    {length: 50},
+    { length: 50 },
     () =>
       `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
   );
 
   private _mobileQueryListener: () => void;
@@ -32,11 +32,27 @@ export class PrivateComponent implements OnInit, OnDestroy{
   }
 
   ngOnInit(): void {
-    this.titulo = 'ola'      
+    this.titulo = 'ola';
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
+  minimize(isMaximized: boolean) {
+    let list = document.getElementsByClassName('mat-list-base');
+    //minimize sidenav when clicking the button if sidenav is maximized
+    if (isMaximized) {
+      for (let i = 0; i < list.length; i++) {
+        list[i].classList.add('mat-list-minimize');
+        //console.log(i);
+      }
+    } else {
+      for (let i = 0; i < list.length; i++) {
+        list[i].classList.remove('mat-list-minimize');
+        //console.log(i);
+      }
+    }
+    this.isMaximized = !isMaximized;
+  }
 }
